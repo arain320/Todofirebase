748dda040755f8e077d079df24cee7a75a94da5e
M Hasnain Saleem <husnainsaleem3512@gmail.com>
2023-07-12 18:26:18 -0700

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import { AuthUserProvider } from "@/firebase/auth";
 import "@/styles/globals.css";
 import Head from "next/head";
+import favicon from "../public/todo.png";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>Hasnain Todo App</title>
+        <link rel="icon" href={favicon.src} sizes="any" />
       </Head>
       <AuthUserProvider>
         <Component {...pageProps} />
